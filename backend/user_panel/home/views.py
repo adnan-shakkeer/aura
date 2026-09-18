bf3fb1e7c89f6828fb4fd90e68991cad3771b3aa
Mohammed Adnan Shakkeer <adnanadnu125@gmail.com>
2026-09-18 10:33:35 +0530

    Implemented Home page setup

--- backend/user_panel/home/views.py
+++ backend/user_panel/home/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from http import HTTPStatus
+
+
+
+def home(request):
+    """
+    Display the AURA home page.
+    """
+
+    return render(
+        request,
+        "home/home.html",
+    )
+
+
+
 
 # Create your views here.
